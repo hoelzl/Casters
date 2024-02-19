cc2b468eecb27cec78839bc82c092dd47afd20e2
Matthias Hölzl <tc@xantira.com>
2024-02-19 23:24:40 +0100

    Simplify GameComponent presentation

--- packages/casters_web/src/web/GameComponent.tsx
+++ packages/casters_web/src/web/GameComponent.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Action } from "./exports";
 import { GameState } from "./GameState";
-import { Resolver, ResolverFun } from "./SelectActionUsingReact";
+import { Resolver } from "./SelectActionUsingReact";
 
 function createButtons(actions: Action[], resolver: Resolver) {
   return actions.map((action: Action, index: number) => (
@@ -19,27 +19,10 @@ type GameComponentProps = {
 export const GameComponent = ({ gameState, resolver }: GameComponentProps) => {
   return (
     <div>
-      <h1>Game</h1>
-      <h2>Props</h2>
-      <ul>
-        <li>gameState: {`${gameState}`}</li>
-        <li>resolver: {`(${resolver.resolve}, {resolver.promise})`}</li>
-      </ul>
-      <h2>Available Actions</h2>
-      Number of Actions: {gameState.availableActions.length}
-      <table>
-        <tbody>
-          {gameState.availableActions.map((action: Action, index: number) => (
-            <tr key={index}>
-              <td>{action.constructor.name}</td>
-              <td>{action.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1>Casters RPG</h1>
       <h2>Current Location: {gameState.currentLocation.name}</h2>
       <p>{gameState.currentLocation.description}</p>
-      <h2>Buttons</h2>
+      <h2>Actions</h2>
       {createButtons(gameState.availableActions, resolver)}
     </div>
   );
